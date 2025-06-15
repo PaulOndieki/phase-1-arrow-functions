975744058b934630e4f3e57d818d87965b25b794
PaulOndieki <ondiekipaulo.17@gmail.com>
2025-06-15 09:41:23 +0300

    completes lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000/100;
+}
+console.log(divide());
+
+const square = x => x*x ;
+console.log(square(5));
+
+const add = (x,y) => x + y ;
+console.log(add(5,6))
